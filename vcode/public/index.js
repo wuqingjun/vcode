d7ca68517393cc8e6731460fe7ce601d18cb50f0
wadewu <wuqingjun@gmail.com>
2015-12-29 12:48:47 -0800

    refactored position code.

--- vcode/public/index.js
+++ vcode/public/index.js
@@ -33,6 +33,7 @@ window.addEventListener('DOMContentLoaded', function () {
         var cx = multiply(origin, matrixX);
         var cy = multiply(cx, matrixY);
         var cz = multiply(cy, matrixZ);
+        return cz;
     } 
    
     function Element(v, parent, increaseshoworder, shape, highlighted) {
@@ -40,19 +41,21 @@ window.addEventListener('DOMContentLoaded', function () {
         this.parent = parent || root;
         this.shape = shape || this.parent.shape;
         this.highlighted = highlighted || false;
-        this.dimension = { height: 1, width: 1 };
+        this.dimension = { x: 1, y: 1, z: 1 };
         this.showorder = increaseshoworder !== false ? ++root.predefinedshoworder : root.predefinedshoworder;
         this.position = { x: this.parent.current.x, y: this.parent.current.y, z: this.parent.current.z };
+        this.rotation = { x: 0, y: 0, z: 0 };
         this.CalculatePosition();
     }
     
     Element.prototype.rotate = function (x, y, z) {
+        this.rotation = {x: x * Math.PI / 180, y: y * Math.PI / 180, z: z * Math.PI / 180};
         var ori = [[this.position.x, this.position.y, this.position.z]];
-        var c = rotate(ori, x * Math.PI / 180, y * Math.PI / 180, z * Math.PI / 180);
+        var c = rotate(ori, this.rotation.x, this.rotation.y, this.rotation.z);
         this.position = { x: c[0][0], y: c[0][1], z: c[0][2] };
     }
     
-    Element.prototype.position = function (x, y, z) {
+    Element.prototype.locate = function (x, y, z) {
         this.position = { x: x, y: y, z: z };
     }
     
@@ -61,13 +64,13 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     
     Element.prototype.CalculatePosition = function (){
-        this.parent.current.x += this.dimension.width;    
+        this.parent.current.x += this.dimension.x;    
     }
 
     Element.prototype.Draw = function (scene) {
+        this.mesh = null;
         if (this.showorder !== null && this.showorder <= root.globalshoworder) {
             var mat = new BABYLON.StandardMaterial("element", scene);
-            this.mesh = null;
             if (this.shape == 'disc') {
                 this.mesh = BABYLON.Mesh.CreateDisc("element", 0.5, 40, scene, false, 2);
                 this.mesh.rotation = new BABYLON.Vector3(Math.PI, 0, 0);
@@ -87,16 +90,13 @@ window.addEventListener('DOMContentLoaded', function () {
                 new BABYLON.Vector3(+0.5, -0.5, 0),
                 new BABYLON.Vector3(-0.5, -0.5, 0)], scene);
             lines.parent = this.mesh;
+            //this.mesh.rotation = new BABYLON.Vector3(this.rotation.x, this.rotation.y, this.rotation.z);
             this.mesh.position.x = this.position.x;
             this.mesh.position.y = this.position.y;
             this.mesh.position.z = this.position.z;
-            //this.CalculatePosition();
-            return this.mesh;
         }
-        return null;
+        return this.mesh;
     }
-    
-
     
     function NewLine(v){
         this.value = v || 1;
@@ -123,75 +123,69 @@ window.addEventListener('DOMContentLoaded', function () {
     NewColumn.prototype.Draw = function (scene){
     }
     
-    //List = function (eles, onebyone, highlighted, shape) {
-    //    this.elements = eles || [];
-    //    this.highlighted = highlighted || false;
-    //    this.showorder = ++predefinedshoworder;
-    //    var x = 0;
-    //    for (var i = 0; i < this.elements.length; ++i, predefinedshoworder += onebyone === true) {
-    //        this.elements[i].showorder = predefinedshoworder;
-    //        this.elements[i].x = i;
-    //        this.elements[i].shape = shape || this.elements[i].shape;
-    //    }
-    //    this.shape = shape || 'square';
-    //    this.x = globalx;
-    //    this.y = globaly;
-    //    this.z = globalz;
-    //}
+    List = function (eles, parent, onebyone, highlighted, shape) {
+        this.elements = eles || [];
+        this.parent = parent || root;
+        this.shape = shape || this.parent.shape;
+        this.highlighted = highlighted || false;
+        this.showorder = ++predefinedshoworder;
+        this.dimension = { x: 0, y: 0, z: 0 };
+        var x = 0;
+        for (var i = 0; i < this.elements.length; ++i, root.predefinedshoworder += onebyone === true) {
+            this.elements[i].showorder = root.predefinedshoworder;
+            this.elements[i].x = i;
+            this.elements[i].shape = this.shape;
+            this.dimension.width += 1;
+        }
+        this.position = { x: this.parent.current.x, y: this.parent.current.y, z: this.parent.current.z };
+        this.CalculatePosition();
+    }
     
-    //List.prototype.CalculatePosition = function (){
-
-    //}
+    List.prototype.CalculatePosition = function (){
+        this.parent.current.x += this.dimension.x;
+    }
     
-    //List.prototype.Add = function(e) {
-    //    e.x = this.elements.length;
-    //    this.elements.push(e);
-    //}
+    List.prototype.Add = function(e) {
+        e.position.x = this.elements.length;
+        this.dimension.width += 1;
+        this.parent.current.x += 1;
+        this.elements.push(e);
+    }
     
-    //List.prototype.Draw = function (scene){
-    //    this.mesh = null;
-    //    if (this.showorder !== null && this.showorder <= globalshoworder) {
-    //        this.mesh = BABYLON.Mesh.CreateLines('listline', 
-    //                                        [new BABYLON.Vector3(-0.5, -0.5, 0), new BABYLON.Vector3(-0.5, 0.5, 0)], 
-    //                                        scene);
-    //        for (var i = 0; i < this.elements.length; ++i) {
-
-    //            var e = this.elements[i].Draw(scene);
-    //            if (e !== null) {
-    //                e.parent = this.mesh;
-    //            }
-    //        }
-    //        this.mesh.position.x = this.x;
-    //        this.mesh.position.y = this.y;
-    //        this.mesh.position.z = this.z;
-    //        this.CalculatePosition();
-    //    }
-    //    return this.mesh;
-    //}
+    List.prototype.Draw = function (scene){
+        this.mesh = null;
+        if (this.showorder !== null && this.showorder <= root.globalshoworder) {
+            for (var i = 0; i < this.elements.length; ++i) {
+                var e = this.elements[i].Draw(scene);
+                if (this.mesh === null) {
+                    this.mesh = e;
+                }
+                else {
+                    e.parent = this.mesh;
+                }
+            }
+        }
+        return this.mesh;
+    }
     
-    //List.prototype.position = function (x, y, z) {
-    //    this.x = x;
-    //    this.y = y;
-    //    this.z = z;
-    //}
+    List.prototype.position = function (x, y, z) {
+        this.position.x = x;
+        this.position.y = y;
+        this.position.z = z;
+    }
     
-    //List.prototype.translate = function (dx, dy, dz) {
-    //    this.translatex = dx;
-    //    this.translatey = dy;
-    //    this.translatez = dz;
-    //}
+    List.prototype.translate = function (dx, dy, dz) {
+        this.position.x += dx;
+        this.position.y += dy;
+        this.position.z += dz;
+    }
     
-    //List.prototype.rotate = function (rotatex, rotatey, rotatez) {
-    //    this.rotatex = rotatex;
-    //    this.rotatey = rotatey;
-    //    this.rotatez = rotatez;
-    //    //for (var i = 0; i < this.elements.length; ++i) {
-    //    //    this.elements[i].element.rotation = new BABYLON.Vector3(Math.PI, 0, Math.PI * z / 180);
-    //    //}
-    //    //if (this.mesh !== null) {
-    //    //    this.mesh.rotation = new BABYLON.Vector3(0, 0, Math.PI * z / 180);
-    //    //}
-    //}
+    List.prototype.rotate = function (x, y, z) {
+        this.rotation = { x: x * Math.PI / 180, y: y * Math.PI / 180, z: z * Math.PI / 180 };
+        var ori = [[this.position.x, this.position.y, this.position.z]];
+        var c = rotate(ori, this.rotation.x, this.rotation.y, this.rotation.z);
+        this.position = { x: c[0][0], y: c[0][1], z: c[0][2] };
+    }
 
     var globalqueue = [];
 	var total = 0;
@@ -210,8 +204,13 @@ window.addEventListener('DOMContentLoaded', function () {
     //var l = [new Element(0), new Element(1)];
     //l.push(new Element(2));
     //globalqueue.push(new List(l, false, null, shape = 'disc'));
-    globalqueue.push(new Element(0, false));
-    globalqueue.push(new Element(1, false));
+    var e0 = new Element(0, false);
+    var e1 = new Element(1, false)
+    e1.locate(2, 1, 0);
+    e1.rotate(0, 0, 90);
+
+    globalqueue.push(e0);
+    globalqueue.push(e1);
     //globalqueue.push(new NewLine(2));
     //globalqueue.push(new Element(2));
     //NewLine();
